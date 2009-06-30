41348b06e1021b8b977fb8f1df56c83aa09c70b9
Adam Sanderson <netghost@gmail.com>
2009-06-30 07:54:12 -0700

    LineNumberingProcessor now actually assigns the correct line numbers.  Method for picking them is... functional...

--- lib/sexp_path/line_numbering_processor.rb
+++ lib/sexp_path/line_numbering_processor.rb
@@ -11,6 +11,8 @@ class LineNumberingProcessor < SexpProcessor
     code = File.read(path)
     sexp = Sexp.from_array(ParseTree.new(true).parse_tree_for_string(code, path).first)
     processor = LineNumberingProcessor.new
+    sexp.line = 0
+    sexp.file = path
     processor.rewrite sexp
   end
   
@@ -18,29 +20,27 @@ class LineNumberingProcessor < SexpProcessor
     super
     self.auto_shift_type = true
     @unsupported.delete :newline
-    
-    @file = ''
-    @line = 0
+    @env = Environment.new
   end
   
   def rewrite exp
-    #sexp = Sexp.from_array(exp) unless Sexp === exp or exp.nil?
     unless exp.nil?
-      exp.file = @file
-      exp.line = @line
+      if exp.sexp_type == :newline
+        @line = exp[1]
+        @file = exp[2]
+      end
+      
+      exp.file ||= @file
+      exp.line ||= @line
     end
-    
     
     super exp
   end
   
-  #s(:newline, 21, "test/sample.rb", s(:call, nil, :private, s(:arglist)) )
   def rewrite_newline(exp)
-    type = exp.shift
-    @line = exp.shift
-    @file = exp.shift
-    sexp = exp.shift
-    
+    # New lines look like:
+    #  s(:newline, 21, "test/sample.rb", s(:call, nil, :private, s(:arglist)) )
+    sexp = exp[3]
     rewrite(sexp)
   end
 end
--- test/line_numbering_processor_test.rb
+++ test/line_numbering_processor_test.rb
@@ -5,16 +5,51 @@ require 'parse_tree'
 class LineNumberingProcessorTest < Test::Unit::TestCase  
   def setup
     @path = File.dirname(__FILE__) + '/sample.rb'
-    #sample = File.read(path)
-    #@sexp = Sexp.from_array(ParseTree.new.parse_tree_for_string(sample, path))
   end
   
   def test_processing_a_file_fills_in_all_the_line_numbers
     sexp = LineNumberingProcessor.process_file(@path)
     assert !sexp.empty?
     sexp.search_each(Q?{_}) do |s| 
-      assert !sexp.line.nil?
-      assert !sexp.file.nil?
+      assert !sexp.line.nil?, "Expected a line number for: #{s.sexp.inspect}"
+      assert !sexp.file.nil?, "Expected a file for: #{s.sexp.inspect}"
     end
+  end
+  
+  # This test may break if sample.rb changes
+  def test_finding_known_lines
+    sexp = LineNumberingProcessor.process_file(@path)
+    lines = open(@path,'r'){|io| io.readlines}
+    
+    assert_line_numbers_equal(
+      lines, 'def test_b()', 
+      sexp,  Q?{ s(:defn, :test_b, _) }
+    )
+    
+    assert_line_numbers_equal(
+      lines, '[apples, oranges, cakes]', 
+      sexp,  Q?{ s(:array, s(:lvar, :apples), s(:lvar, :oranges), s(:lvar, :cakes)) }
+    )
+    
+    assert_line_numbers_equal(
+      lines, "require 'test/unit'",
+      sexp, Q?{ s(:fcall, :require, s(:array, s(:str, "test/unit"))) }
+    )
+  end
+  
+  private
+  def assert_line_numbers_equal(lines, code, sexp, pattern)
+    string_line = find_line(lines, code)
+    sexp_line =   (sexp / pattern).first.sexp.line
+    
+    assert_equal string_line, sexp_line, "Expected to find #{code} at line #{string_line}"
+  end
+  
+  def find_line(lines, code)
+    
+    lines.each_with_index do |line,i|
+      return i+1 if line.index(code) # 1 based indexing
+    end
+    return nil
   end
 end
